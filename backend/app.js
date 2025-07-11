91fb511fa433d50c027c6be43f13844de2881190
TauaRibeiro <tataumani@gmail.com>
2025-07-11 08:39:55 -0300

    UPDATE: app.js
    Fazendo com que seja possível rodar o servidor e testando a conexão do banco

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,23 @@
-const DATABASE_URL = process.env.DATABASE_URL
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-console.log(DATABASE_URL)
+const initDatabase = require('./config/mongooseConfig');
+const { init } = require('./models/Status');
+
+const app = express();
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+
+
+app.listen(port, () => {
+    try{
+        initDatabase()
+        console.log(`Servidor rodando em http://127.0.0.1:${port}/`)    
+    }catch(error){
+        console.error("Erro ao inicializar o servidor...")
+    }
+})
