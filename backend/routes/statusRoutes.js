d16a068f555f4589ab343076c23aeb6a5c2ce613
TauaRibeiro <tataumani@gmail.com>
2025-07-15 08:15:59 -0300

    CREATE: statusRoutes.js
    Criando as rotas para uso

---
+++ backend/routes/statusRoutes.js
@@ -0,0 +1,11 @@
+const statusController = require('../controllers/statusController')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', statusController.getAll)
+router.get('/:id', statusController.getById)
+router.post('/', statusController.create)
+router.put('/:id', statusController.update)
+router.delete('/:id', statusController.delete)
+
+module.exports = router
