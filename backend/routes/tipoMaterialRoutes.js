f594c117255e9fbd0af12f38de9c6b97a32ccb10
TauaRibeiro <tataumani@gmail.com>
2025-07-21 08:41:53 -0300

    CREATE: tipoMaterialRoutes.js
    Criação das rotas do tipoMaterial

---
+++ backend/routes/tipoMaterialRoutes.js
@@ -0,0 +1,11 @@
+const tipoMaterialController = require('../controllers/tipoMaterialController')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', tipoMaterialController.getAll)
+router.get('/:id', tipoMaterialController.getByID)
+router.post('/', tipoMaterialController.create)
+router.patch('/:id', tipoMaterialController.update)
+router.delete('/:id', tipoMaterialController.delete)
+
+module.exports = router
