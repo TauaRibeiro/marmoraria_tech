90acba6722622eec24aff901f1625cab72ea61cf
TauaRibeiro <tataumani@gmail.com>
2025-08-01 17:42:39 -0300

    UPDATE: utils/
    Atualizando os utils

--- backend/utils/validarData.js
+++ backend/utils/validarData.js
@@ -9,7 +9,15 @@ module.exports = (date) => {
     const mes = matches[2]-1;
     const ano = matches[3];
 
+    if(parseInt(ano) > new Date().getFullYear()){
+        return false
+    }
+
     const data = new Date(ano, mes, dia);
     
-    return data.getDate() == dia && data.getMonth() == mes && data.getFullYear() == ano;
+    if(data.getDate() == dia && data.getMonth() == mes && data.getFullYear() == ano){
+        return data
+    }
+
+    return false
 }
--- backend/utils/validarEmail.js
+++ backend/utils/validarEmail.js
@@ -1,3 +1,3 @@
 module.exports = (email) => {
-    return /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.[a-z]?$/i.test(email)
+    return /^[a-z0-9.]+@[a-z0-9]+\.[a-z]/i.test(email)
 }
