bfd4704a191eb054eb9621a82279f92ce9155c3c
TauaRibeiro <tataumani@gmail.com>
2025-08-04 09:55:06 -0300

    CREATE: authService.js
    Criação do auth de login de usuário

---
+++ backend/services/authService.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken')
+const Funcionario = require('../models/Funcionario')
+const SECRET = process.env.SECRET
+const validarEmail = require('../utils/validarEmail')
+
+exports.login = async (data) => {
+    try{
+        if(!data.id){
+            return {status: 400, message: "Id é obrigatório"}
+        }
+    
+        if(data.id.trim().length === 0 || data.id.length < 24 || data.id.length > 24){
+            return {status: 400, message: "Id inválido"}
+        }
+    
+        const {email, senha} = data
+    
+        if(!email || !senha){
+            return {status: 400, message: "Email e senha são obrigatórios para o login"}
+        }
+    
+        if(!validarEmail(email)){
+            return {status: 400, message: "Email inválido"}
+        }
+    
+        if(senha.length <= 2){
+            return {status: 400, message: "Senha inválida"}
+        }
+    
+        const usuario = await Funcionario.findOne({email, senha})
+    
+        if(!usuario){
+            return {status: 404, message: "Usuario não encontrado"}
+        }
+
+        const token = jwt.sign({id: usuario._id, login: usuario.email}, SECRET, { expiresIn: "12h"})
+
+        return {status: 200, result: token}
+    }catch(error){
+        console.error("Erro ao fazer login do usuario: ", error)
+    }
+}
