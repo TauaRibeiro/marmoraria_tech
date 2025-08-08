1d796456edb187dc46592a93fc1ed0731823f4e0
TauaRibeiro <tataumani@gmail.com>
2025-08-08 08:13:53 -0300

    UPDATE: material.Routes.js
    Adicionado as rotas para o get by id e o delete

--- backend/routes/materialRoutes.js
+++ backend/routes/materialRoutes.js
@@ -3,6 +3,8 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', materialController.getAll)
+router.get('/:id', materialController.getById)
 router.post('/', materialController.create)
+router.delete('/:id', materialController.delete)
 
 module.exports = router
