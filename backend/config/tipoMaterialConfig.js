44c834953db30ae27e78ea780ac08cfc2fec00b5
TauaRibeiro <tataumani@gmail.com>
2025-08-11 16:49:43 -0300

    CREATE: tipoMaterialConfig.js
    Configuração inicial que irá verificar se os tipos padrões foram criados

---
+++ backend/config/tipoMaterialConfig.js
@@ -0,0 +1,15 @@
+const TipoMaterial = require('../models/TipoMaterial')
+
+module.exports = async () => {
+    const tiposPadroes = ['Tipo Deletado']
+
+    tiposPadroes.map(async (padrao) => {
+        let result = await TipoMaterial.findOne({nome: padrao})
+
+        if(!result){
+            result = await TipoMaterial.create({nome: padrao}) 
+        }
+
+        process.env[padrao.toUpperCase().replace(' ', '_')] = result._id
+    })
+}
