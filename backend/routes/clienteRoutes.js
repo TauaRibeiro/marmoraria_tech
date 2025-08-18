49a9c028a58e94a7da5bd060df7a2b9c30f62f9f
TauaRibeiro <tataumani@gmail.com>
2025-08-18 11:02:38 -0300

    UPDATE: clienteRoutes.js
    Adicionando a rota get de cliente

--- backend/routes/clienteRoutes.js
+++ backend/routes/clienteRoutes.js
@@ -3,5 +3,6 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', clienteController.create)
+router.get('/', clienteController.getAll)
 
 module.exports = router
