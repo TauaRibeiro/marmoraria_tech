323551155b78c93751bdb0f6f9b01b82661db2f9
TauaRibeiro <tataumani@gmail.com>
2025-08-25 08:35:08 -0300

    UPDATE: clienteRoutes.js
    Adicionando a rota delete de cliente

--- backend/routes/clienteRoutes.js
+++ backend/routes/clienteRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 router.post('/', clienteController.create)
 router.get('/', clienteController.getAll)
 router.get('/:id', clienteController.getByID)
+router.delete('/:id', clienteController.delete)
 
 module.exports = router
