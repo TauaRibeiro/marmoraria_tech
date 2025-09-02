af1bc638da92486784c79cfa33bce27ac2069288
TauaRibeiro <tataumani@gmail.com>
2025-09-02 17:01:00 -0300

    UPDATE: precoMaterialRoutes.js
    Adicionando a rota getById

--- backend/routes/precoMaterialRoutes.js
+++ backend/routes/precoMaterialRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 router.post('/', precoMaterialController.create)
 router.get('/', precoMaterialController.getAll)
 router.get('/efetivado', precoMaterialController.get)
+router.get('/:id', precoMaterialController.getById)
 
 module.exports = router
