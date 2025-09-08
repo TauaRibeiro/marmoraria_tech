cc7c9c7aa90548029b28cf7d7a441692d494e680
TauaRibeiro <tataumani@gmail.com>
2025-09-08 19:51:28 -0300

    UPDATE: orcamentoController.js
    Adicionando a requisição get de orçamento

--- backend/controllers/orcamentoController.js
+++ backend/controllers/orcamentoController.js
@@ -9,3 +9,13 @@ exports.create = async (req, res) => {
 
     return res.status(resultado.status).json({message: resultado.message})
 }
+
+exports.get = async (req, res) => {
+    const resultado = await orcamentoService.getOrcamento()
+
+    if(resultado.status === 200){
+        return {status: 200, result: resultado.result}
+    }
+
+    return res.status(resultado.status).json({message: resultado.message})
+}
