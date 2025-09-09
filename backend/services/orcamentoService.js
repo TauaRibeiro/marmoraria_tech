a614717e4d2122dcd42df7189e665f706bc619f6
TauaRibeiro <tataumani@gmail.com>
2025-09-09 08:39:11 -0300

    UPDATE: orcamentoService.js
    Adicionando o método getById de orcamento

--- backend/services/orcamentoService.js
+++ backend/services/orcamentoService.js
@@ -70,3 +70,23 @@ exports.getOrcamento = async () => {
         return {status: 500, message: 'Erro ao fazer o get de Orcamentos'}
     }
 }
+
+exports.getOrcamentoById = async (id) => {
+    try{
+        if(!validarId(id)){
+            return {status: 400, message: "Id inválido"}
+        }
+    
+        const result = await Orcamento.findById(id)
+    
+        if(!result){
+            return {status: 404, message: "Orcamento não encontrado"}
+        }
+
+        return {status: 200, result: {...result, valorTotal: result.valorInstalacao + result.valorPagamento + result.valorFrete}}
+    }catch(error){
+        console.error('Erro ao fazer o get por id de orcamento: ', error)
+        
+        return {status: 500, message: 'Erro ao fazer o get por id de orcamento'}
+    }
+}
