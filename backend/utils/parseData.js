96f0472ca6d36a169d45934f86b31e26f6c78854
TauaRibeiro <tataumani@gmail.com>
2025-09-13 08:43:40 -0300

    UPDATE: parseData.js
    Alterando o nome do útili de validarData.js para paseData.js

--- backend/utils/parseData.js
+++ backend/utils/parseData.js
@@ -1,8 +1,10 @@
+const DataError = require('../models/DataError')
+
 module.exports = (date) => {
     const matches = /(\d{2})[-.\/](\d{2})[-.\/](\d{4})/.exec(date);
     
     if (matches == null) {
-        return false;
+        throw new DataError('Type Error', 400, 'Data inválida')
     }
     
     const dia = matches[1];
@@ -10,17 +12,17 @@ module.exports = (date) => {
     const ano = matches[3];
 
     if(parseInt(ano) > new Date().getFullYear()){
-        return false
+        throw new DataError('Validation Error', 400, 'O ano da data não pode ser maior que a data atual')
     }
 
     const data = new Date(ano, mes, dia);
     
     if(data.getDate() == dia && data.getMonth() == mes && data.getFullYear() == ano){
         if(data > Date.now()){
-            return false
+            throw new DataError('Validation Error', 400, 'A data não pode ser maior que a data atual')
         }
         return data
     }
 
-    return false
+    throw new DataError('Type Error', 400, 'Data inválida')
 }
