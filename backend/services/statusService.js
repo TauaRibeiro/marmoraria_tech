bce6f837ffc345c70d838dd66d531a17af5bbf5b
TauaRibeiro <tataumani@gmail.com>
2025-09-15 17:24:21 -0300

    UPDATE: statusService.js
    Adaptando o service de status para utilizar as classes

--- backend/services/statusService.js
+++ backend/services/statusService.js
@@ -1,120 +1,69 @@
-const Status = require('../models/schemas/Status')
-const materialService = require('../services/materialService')
-const validarId = require('../utils/validarIdMongoose')
+const Status = require('../models/Status')
+const Material = require('../models/Material')
+const Orcamento = require('../models/Orcamento')
+const DataError = require('../models/DataError')
 
 exports.criarStatus = async (nome) => {
     try{
-        if(nome.trim().length === 0){
-            return {status: 400, message: "Nome inválido"}
-        }
-    
-        await Status.create({nome: nome.trim()})
-
-        return {status: 201}
+        const novoStatus = new Status(nome)
+        await novoStatus.create()
     }catch(error){
-        if(error.code === 11000){
-            return {status: 400, message: "Já existe um status com esse nome"}
-        }
-        
-        console.error(`Erro ao criar status: `);
-        console.error(error)
-        return {status: 500, message: `Erro ao criar status`}
+        throw error
     }
 }
 
-exports.getStatus = async () => {
+exports.getAllStatus = async () => {
     try{
-        const result = await Status.find();
-
-        return {status: 200, result}
-    }catch(err){
-        console.error('Erro ao pegar todos os status')
-
-        return {status: 500, message: "Erro ao pegar todos os status"}
+        const resultado = await Status.findAll()
+        
+        return resultado.map((status) => {
+            return {
+                id: status.id,
+                nome: status.nome,
+                createdAt: status.createdAt,
+                updatedAt: status.updatedAt
+            }
+        })
+    }catch(error){
+        throw error
     }
 }
 
 exports.getStatusByID = async (id) => {
     try{
-        if(!validarId(id)){
-            return {status: 400, message: "Id inválido"}
-        }
-        const result = await Status.findById(id.trim());
-
-        if(!result){
-            return {status: 404, message: "Status não encontrado"}
-        }
-
-        return {status: 200, result}
+        const resultado = await Status.findById(id)
+    
+        return {id: resultado.id, nome: resultado.nome, createdAt: resultado.createdAt, updatedAt: resultado.updatedAt}
     }catch(error){
-        console.error(`Erro ao buscar Status com id ${id}: `, error);
-
-        return {status: 500, message: "Erro ao buscar Status"}
+        throw error
     }
 }
 
 exports.updateStatus = async (id, novoNome) => {
-    try {
-        if(!validarId(id)){
-            return {status: 400, message: "Id inválido"}
-        }
-
-        if(novoNome.trim().length == 0){
-            return {status: 400, message: "Nome inválido"}
-        }
-        
-        const statusAntigo = await Status.findById(id.trim())
-
-        if(!statusAntigo){
-            return {status: 404, message: `Status com id ${id} não encontrado`}
-        }
-        
-        if(process.env[statusAntigo.nome.toUpperCase().replace(' ', '_')]){
-            return {status: 400, message: "Não é possível alterar um status padrão"}
-        }
-        
-        // const statusAtualizado = await Status.findByIdAndUpdate(id.trim(), {nome: novoNome.trim()})
-        
-        await statusAntigo.updateOne({nome: novoNome.trim()})
-
-        return {status: 200}
-    } catch (error) {
-        if(error.codeName == 'DuplicateKey' || error.code === 11000){
-            return {status: 400, message: "Já existe um status com esse nome"}
-        }
-
-        console.error(`Erro ao atualizar status: `, error);
-        return {status: 500, message: "Erro ao atualizar status" }
+    try{
+        const statusAntigo = await Status.findById(id)
+    
+        statusAntigo.nome = novoNome
+    
+        await statusAntigo.update()
+    }catch(error){
+        throw error
     }
 }
 
 exports.deleteStatus = async (id) => {
-    try {
-        if(!validarId(id)){
-            return {status: 400, message: "Id inválido"}
-        }
+    try{
+        const status = await Status.findById(id)
         
-        const statusDeletado = await Status.findById(id.trim())
-              
-        if(!statusDeletado){
-            return {status: 404, message: "Status não encontrado"}
+        if(process.env[padrao.toUpperCase().replace(' ', '_')]){
+            throw new DataError('Validation error', 400, 'Não é possível deletar um status padrão')
         }
 
-        if(process.env[statusDeletado.nome.toUpperCase().replace(' ', '_')]){
-            return {status: 400, message: "Não é possível deletar um status padrão"}
-        }
-        const resultadoMaterialService = await materialService.updateMaterialBy({idStatus: id.trim()},{idStatus: process.env.STATUS_DELETADO})
-        
-        if(resultadoMaterialService.status !== 200 && resultadoMaterialService.status !== 404){
-            return {status: resultadoMaterialService.status, message: resultadoMaterialService.message} 
-        }
-        
-        await statusDeletado.deleteOne()
+        await Material.updateManyBy({idStatus: status.id}, {idStatus: process.env.STATUS_DELETADO})
+        await Orcamento.updateManyBy({idStatus: status.id}, {idStatus: process.env.STATUS_DELETADO})
 
-        return {status: 200}
+        status.delete()
     }catch(error){
-        console.error(`Erro ao deletar os Status: `, error)
-
-        return {status: 500, message: "Erro ao deletar os status"}
+        throw error
     }
 }
