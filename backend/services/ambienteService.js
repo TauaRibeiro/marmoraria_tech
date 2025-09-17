5dddf9a1c260b401c913c4a0b173f9428bf2f89b
TauaRibeiro <tataumani@gmail.com>
2025-09-17 14:00:04 -0300

    UPDATE: ambienteService.js
    Adaptando o service para utilizar a classe

--- backend/services/ambienteService.js
+++ backend/services/ambienteService.js
@@ -1,97 +1,79 @@
 const Ambiente = require('../models/Ambiente')
+const DataError = require('../models/DataError')
 const validarId = require('../utils/validarIdMongoose')
 
 exports.criarAmbiente = async (nome) => {
     try{
-        if(nome.trim().length === 0){
-            return {status: 400, message: "Nome inválido"}
-        }
-    
-        const result = await Ambiente.create({nome: nome.trim()})
+        const novoAmbiente = new Ambiente(nome)
 
-        return {status: 201, result}
+        await novoAmbiente.create()
     }catch(error){
-        if(error.codeName == 'DuplicateKey' || error.code === 11000){
-            return {status: 400, message: "Já existe um ambiente com esse nome"}
-        }
-        console.error(`Erro ao criar ambiente ${nome}: ${error}`)
-        return {status: 500, message: `Erro ao criar ambiente ${nome}`}
+        throw error
     }
 }
 
 exports.getAmbiente = async () => {
     try{
-        const result = await Ambiente.find();
+        const result = await Ambiente.findAll()
 
-        return {status: 200, result}
-    }catch(err){
-        console.error('Erro ao pegar todos os ambientes')
-
-        return {status: 500, message: "Erro ao pegar todos os ambientes"}
+        return result.map((ambiente) => {
+            return {
+                id: ambiente.id,
+                nome: ambiente.nome,
+                createdAt: ambiente.createdAt,
+                updatedAt: ambiente.updatedAt
+            }
+        })
+    }catch(error){
+        throw error
     }
 }
 
 exports.getAmbienteByID = async (id) => {
     try{
-        if(!validarId(id)){
-            return {status: 400, message: "Id inválido"}
-        }
-        const result = await Ambiente.findById(id.trim());
+        const ambiente = await Ambiente.findById(id)
 
-        if(!result){
-            return {status: 404}
+        if(!ambiente){
+            throw new DataError('Not Found', 404, 'Ambiente não encontrado')
         }
 
-        return {status: 200, result}
+        return {
+            id: ambiente.id,
+            nome: ambiente.nome,
+            createdAt: ambiente.createdAt,
+            updatedAt: ambiente.updatedAt
+        }
     }catch(error){
-        console.error(`Erro ao buscar Ambiente com id ${id}: `, error);
-
-        return {status: 500, message: "Erro ao buscar Ambiente"}
+        throw error
     }
 }
 
 exports.updateAmbiente = async (id, novoNome) => {
     try {
-        if(!validarId(id)){
-            return {status: 400, message: "Id inválido"}
+        const ambiente = await Ambiente.findById(id)
+
+        if(!ambiente){
+            throw new DataError('Not Found', 404, 'Ambiente não encontrado')
         }
 
-        if(novoNome.trim().length == 0){
-            return {status: 400, message: "Nome inválido"}
-        }
-        
-        const statusAtualizado = await Ambiente.findByIdAndUpdate(id, {nome: novoNome.trim()})
-        
-        if(!statusAtualizado){
-            return {status: 404, message: `Ambiente com id ${id} não encontrado`}
-        }
+        ambiente.nome = novoNome
 
-        return {status: 200}
+        await ambiente.update()
     } catch (error) {
-        if(error.codeName == 'DuplicateKey' || error.code === 11000){
-            return {status: 400, message: "Já existe um ambiente com esse nome"}
-        }
-        console.error(`Erro ao atualizar ambiente: `, error);
-        return {status: 500, message: "Erro ao atualizar ambiente" }
+        throw error
     }
 }
 
 exports.deleteAmbiente = async (id) => {
     try {
-        if(!validarId(id)){
-            return {status: 400, message: "Id inválido"}
+        const ambiente = await Ambiente.findById(id)
+
+        if(!ambiente){
+            throw new DataError('Not Found', 404, 'Ambiente não encontrado')
         }
 
-        const statusDeletado = await Ambiente.findByIdAndDelete(id.trim())
-        
-        if(!statusDeletado){
-            return {status: 404, message: "Ambiente não encontrado"}
-        }
-
-        return {status: 200}
+        await ambiente.delete()
     }catch(error){
-        console.error(`Erro ao deletar o Ambiente: ${id}`)
-
-        return {status: 500, message: "Erro ao deletar o ambiente"}
+        throw error
     }
 }
