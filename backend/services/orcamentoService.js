acc0a0e3803a12ec6b8de2271d719ec7bda2e228
TauaRibeiro <tataumani@gmail.com>
2025-09-25 20:51:34 -0300

    UPDATE: orcamentoService.js
    Início da adaptação do service de orçamento

--- backend/services/orcamentoService.js
+++ backend/services/orcamentoService.js
@@ -1,57 +1,25 @@
 const Orcamento = require('../models/Orcamento')
 const Cliente = require('../models/Cliente')
 const Status = require('../models/Status')
-const validarId = require('../utils/validarIdMongoose')
-const eNumerico = require('../utils/eNumerico')
-
+const Ambiente = require('../models/Ambiente')
+const PrecoMaterial = require('../models/PrecoMaterial')
+const Material = require('../models/Material')
+const ItemOrcamento = require('../models/Orcamento')
+const Cliente = require('../models/Cliente')
+const Status = require('../models/Status')
 
 exports.createOrcamento = async (data) => {
     try {
-        let {idCliente, idStatus, valorPagamento, valorFrete, valorInstalacao} = data
+        let {idCliente, idStatus, valorFrete, valorInstalacao, itens} = data
+        let valorPagamento = 0
+        
+        if(!(await Cliente.find)){}
 
-        if(!idCliente || !idStatus || !valorPagamento){
-            return {status: 400, message: "Os campos idCliente, idStatus, valorPagamento e valorFrete ou valorInstalacao são obrigatórios"}
-        }
-
-        if(!validarId(idCliente) || !validarId(idStatus)){
-            return {status: 400, message: "Id inválido de cliente e/ou status"}
-        }
-
-        if(!(await Cliente.findById(idCliente))){
-            return {status: 404, message: "Cliente não encontrado"}
-        }
-
-        if(!(await Status.findById(idStatus))){
-            return {status: 404, message: "Status não encontrado"}
-        }
-
-        valorFrete = (valorFrete) ? valorFrete : 0
-        valorInstalacao = (valorInstalacao) ? valorInstalacao : 0
-
-        if(!eNumerico(valorFrete) || !eNumerico(valorPagamento) || !eNumerico(valorInstalacao)){
-            return {status: 400, message: "Os valores devem ser um número válido"}
-        }
-
-        valorFrete = parseFloat(valorFrete)
-        valorInstalacao = parseFloat(valorInstalacao)
-        valorPagamento = parseFloat(valorPagamento)
-
-        if(valorFrete < 0 || valorInstalacao < 0 || valorPagamento < 0){
-            return {status: 400, message: "Os valores devem ser maiores ou iguais a zero"}
-        }
-
-        await Orcamento.create({
-            idCliente: idCliente.trim(),
-            idStatus: idStatus.trim(),
-            valorPagamento: valorPagamento.toFixed(2),
-            valorFrete: valorFrete.toFixed(2),
-            valorInstalacao: valorInstalacao.toFixed(2)
-        })
-
-        return {status: 201}
+        const novoOrcamento = new Orcamento(idCliente, idStatus, valorPagamento, valorFrete, valorInstalacao)
+        
+        
     } catch (error) {
-        console.error('Erro ao criar orcamento: ', error)
-        return {status: 500, message: "Erro ao criar orcamento"}
+        throw error
     }
 }
 
