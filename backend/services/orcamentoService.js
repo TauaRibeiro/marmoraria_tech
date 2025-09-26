2c0519d78f8ac875e9f9f3ad39ae84d42822230a
TauaRibeiro <tataumani@gmail.com>
2025-09-26 17:49:19 -0300

    UPDATE: orcamentoService.js
    Adaptando os métodos createOrcamento, getOrcamento, getOrcamentoById e updateOrcamento

--- backend/services/orcamentoService.js
+++ backend/services/orcamentoService.js
@@ -4,20 +4,57 @@ const Status = require('../models/Status')
 const Ambiente = require('../models/Ambiente')
 const PrecoMaterial = require('../models/PrecoMaterial')
 const Material = require('../models/Material')
-const ItemOrcamento = require('../models/Orcamento')
+const ItemOrcamento = require('../models/ItemOrcamento')
 const Cliente = require('../models/Cliente')
 const Status = require('../models/Status')
+const DataError = require('../models/DataError')
 
 exports.createOrcamento = async (data) => {
     try {
         let {idCliente, idStatus, valorFrete, valorInstalacao, itens} = data
         let valorPagamento = 0
         
-        if(!(await Cliente.find)){}
+        if(!(await Cliente.findById(idCliente))){
+            throw new DataError('Not Found', 404, 'Cliente não encontrado')
+        }
+
+        if(!(await Status.findById(idStatus))){
+            throw new DataError('Not Found', 404, 'Status não encontrado')
+        }
 
         const novoOrcamento = new Orcamento(idCliente, idStatus, valorPagamento, valorFrete, valorInstalacao)
+        await novoOrcamento.create()
+
+        itens = itens.map(async (item) => {
+            const { idAmbiente, idMaterial, quantidadeItem, comprimentoItem, larguraItem } = item
+
+            if(!(await Ambiente.findById(idAmbiente))){
+                throw new DataError('Not Found', 404, 'Ambiente não encontrado')
+            }
+
+            if(!(await Material.findById(idMaterial))){
+                throw new DataError('Not Found', 404, 'Material não encontrado')
+            }
+
+            const preco = (await PrecoMaterial.findCurrentPrices({idMaterial}))[0]
+
+            const novoItem = new ItemOrcamento(novoOrcamento.id, idAmbiente, idMaterial, preco.id, quantidadeItem, comprimentoItem, larguraItem)
+            await novoItem.create()
+
+            valorPagamento += comprimentoItem*larguraItem*preco.valorMaterial*quantidadeItem
+
+            return JSON.parse(JSON.stringify(novoItem))
+        })
+
+        novoOrcamento.valorPagamento = valorPagamento
+        await novoOrcamento.update()
         
-        
+        const jsonOrcamento = JSON.parse(JSON.stringify(novoOrcamento))
+
+        return {...jsonOrcamento, 
+            itens, 
+            valorTotal: novoOrcamento.valorPagamento + novoOrcamento.valorFrete + novoOrcamento.valorInstalacao
+        }
     } catch (error) {
         throw error
     }
@@ -25,119 +62,112 @@ exports.createOrcamento = async (data) => {
 
 exports.getOrcamento = async () => {
     try{
-        let result = await Orcamento.find()
+        const orcamentos = await Orcamento.findAll()
+        const itens = orcamentos.map(async (orcamento) => {
+            const resultado = await ItemOrcamento.findManyBy({idOrcamento: orcamento.id})
 
-        const newResult = result.map((orcamento) => {
-            const {_id, idCliente, idStatus, valorPagamento, valorFrete, valorInstalacao} = orcamento
-
-            return {
-                _id,
-                idCliente,
-                idStatus,
-                valorPagamento,
-                valorFrete,
-                valorInstalacao,
-                valorTotal: valorPagamento+valorFrete+valorInstalacao
-            }
+            return resultado.map((item) => JSON.parse(JSON.stringify(item)))
         })
 
-        return {status: 200, result: newResult}
-    }catch(error){
-        console.error('Erro ao fazer o get de Orcamentos: ', error)
+        const result = []
 
-        return {status: 500, message: 'Erro ao fazer o get de Orcamentos'}
+        for(let i = 0; i < orcamentos.length; i++){
+            const jsonOrcamento = JSON.parse(JSON.stringify(orcamentos[i]))
+
+            result.push({...jsonOrcamento, 
+                itens: itens[i], 
+                valorTotal: orcamentos[i].valorPagamento + orcamentos[i].valorFrete + orcamentos[i].valorInstalacao
+            })
+        }
+
+        return result
+    }catch(error){
+        throw error
     }
 }
 
 exports.getOrcamentoById = async (id) => {
     try{
-        if(!validarId(id)){
-            return {status: 400, message: "Id inválido"}
-        }
-    
-        const result = await Orcamento.findById(id)
-    
-        if(!result){
-            return {status: 404, message: "Orcamento não encontrado"}
+        let orcamento = await Orcamento.findById(id)
+
+        if(!orcamento){
+            throw new DataError('Not Found', 404, 'Orçamento não encontrado')
         }
 
-        const {_id, idCliente, idStatus, valorPagamento, valorFrete, valorInstalacao} = result
+        let itens = await ItemOrcamento.findManyBy({idOrcamento: orcamento.id})
 
-        return {status: 200, result: {
-            _id,
-            idCliente,
-            idStatus,
-            valorPagamento,
-            valorFrete,
-            valorInstalacao,
-            valorTotal: valorPagamento+valorFrete+valorInstalacao
-        }}
-    }catch(error){
-        console.error('Erro ao fazer o get por id de orcamento: ', error)
+        itens = itens.map((item) => {
+            return JSON.parse(JSON.stringify(item))
+        })
+
+        orcamento = JSON.parse(JSON.stringify(orcamento))
         
-        return {status: 500, message: 'Erro ao fazer o get por id de orcamento'}
+        return {...orcamento, 
+            itens,
+            valorTotal: orcamento.valorFrete + orcamento.valorInstalacao + orcamento.valorPagamento
+        }
+    }catch(error){
+        throw error
     }
 }
 
 exports.updateOrcamento = async (data) => {
     try{
-        
-        if(!validarId(data.id)){
-            return {status: 400, message: "Id inválido"}
-        }
-        
-        const antigoOrcamento = await Orcamento.findById(data.id)
+        let { id, idCliente, idStatus, valorPagamento, valorFrete, valorInstalacao, itens } = data
+        const orcamento = await Orcamento.findById(id)
 
-        if(!antigoOrcamento){
-            return {status: 404, message: "Orcamento não encontrado"}
-        }
-        
-        let {idCliente, idStatus, valorPagamento, valorFrete, valorInstalacao} = data
-    
-        if(!idCliente || !idStatus || !valorPagamento || !valorFrete || !valorInstalaca){
-            return {status: 400, message: "Os campos idCliente, idStatus, valorPagamento e valorFrete ou valorInstalacao são obrigatórios"}
-        }
-    
-        if(!validarId(idCliente) || !validarId(idStatus)){
-            return {status: 400, message: "Id inválido de cliente e/ou status"}
-        }
-    
-        if(!(await Cliente.findById(idCliente))){
-            return {status: 404, message: "Cliente não encontrado"}
-        }
-    
-        if(!(await Status.findById(idStatus))){
-            return {status: 404, message: "Status não encontrado"}
-        }
-    
-        valorFrete = (valorFrete) ? valorFrete : 0
-        valorInstalacao = (valorInstalacao) ? valorInstalacao : 0
-    
-        if(!eNumerico(valorFrete) || !eNumerico(valorPagamento) || !eNumerico(valorInstalacao)){
-            return {status: 400, message: "Os valores devem ser um número válido"}
-        }
-    
-        valorFrete = parseFloat(valorFrete)
-        valorInstalacao = parseFloat(valorInstalacao)
-        valorPagamento = parseFloat(valorPagamento)
-    
-        if(valorFrete < 0 || valorInstalacao < 0 || valorPagamento < 0){
-            return {status: 400, message: "Os valores devem ser maiores ou iguais a zero"}
+        if(!orcamento){
+            throw new DataError('Not Found', 404, 'Orçamento não encontrado')
         }
 
-        await antigoOrcamento.updateOne({
-            idCliente: idCliente.trim(),
-            idStatus: idStatus.trim(),
-            valorPagamento: valorPagamento.toFixed(2),
-            valorFrete: valorFrete.toFixed(2),
-            valorInstalacao: valorInstalacao.toFixed(2)
+        let novoValor = 0
+
+        const itensAntigos = await ItemOrcamento.findManyBy({idOrcamento: orcamento.id})
+
+        itens = map(async (item) => {
+            const { idAmbiente, idMaterial, quantidadeItem, comprimentoItem, larguraItem } = item
+
+            if(!(await Ambiente.findById(idAmbiente))){
+                throw new DataError('Not Found', 404, 'Ambiente não encontrado')
+            }
+
+            if(!(await Material.findById(idMaterial))){
+                throw new DataError('Not Found', 404, 'Material não encontrado')
+            }
+
+            const preco = (await PrecoMaterial.findCurrentPrices({idMaterial}))[0]
+
+            novoValor += quantidadeItem*comprimentoItem*larguraItem*preco.valorMaterial
+
+            return new ItemOrcamento(orcamento.id, idAmbiente, idMaterial, preco.id, quantidadeItem, comprimentoItem, larguraItem)
         })
 
-        return {status: 200}
-    }catch(error){
-        console.error('Erro ao atualizar orcamento: ', error)
+        for(let i = 0; i < itensAntigos.length; i++){
+            if(itens.indexOf(itensAntigos[i]) === -1){
+                await itensAntigos[i].delete()
+            }
+        }
 
-        return {status: 500, message: "Erro ao atualizar orcamento"}
+        for(let i = 0; i < itens.length; i++){
+            if(itensAntigos.indexOf(itens[i]) === -1){
+                await itens[i].create()
+            }
+        }
+
+        orcamento.idCliente = idCliente
+        orcamento.idStatus = idStatus
+        orcamento.valorPagamento = valorPagamento
+        orcamento.valorFrete = valorFrete
+        orcamento.valorInstalacao = valorInstalacao
+
+        const jsonOrcamento = JSON.parse(JSON.stringify(orcamento))
+
+        return {...jsonOrcamento, 
+            itens: itens.map((item) => JSON.parse(JSON.stringify(item))),
+            valorTotal: orcamento.valorFrete + orcamento.valorInstalacao + orcamento.valorPagamento
+        }
+    }catch(error){
+        throw error
     }
 }
 
