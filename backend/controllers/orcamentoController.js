baddb2bbdf28fef0c2833965be511acdaf42e156
TauaRibeiro <tataumani@gmail.com>
2025-09-29 13:26:55 -0300

    UPDATE: Adaptando o controller de orcamento

--- backend/controllers/orcamentoController.js
+++ backend/controllers/orcamentoController.js
@@ -1,51 +1,238 @@
 const orcamentoService = require('../services/orcamentoService')
+const validarId = require('../utils/validarIdMongoose')
+const eNumerico = require('../utils/eNumerico')
 
 exports.create = async (req, res) => {
-    const resultado = await orcamentoService.createOrcamento(req.body)
+    try{
+        let {idCliente, idStatus, valorFrete, valorInstalacao, itens} = req.body
 
-    if(resultado.status === 201){
-        return res.sendStatus(201)
+        if(!idCliente || !idStatus || !valorFrete || !valorInstalacao || !itens){
+            return res.status(400).json({name: "Parameter Error", message: "idCliente, idStatus, valorPagamento, valorFrete, valorInstalacao e itens são obrigatóritos"})
+        }
+
+        idCliente = idCliente.trim()
+        idStatus = idStatus.trim()
+        valorPagamento = (typeof(valorPagamento) === 'number') ? valorPagamento.toString() : valorPagamento.trim().replace(',', '.')
+        valorFrete = (typeof(valorFrete) === 'number') ? valorFrete.toString() : valorFrete.trim().replace(',', '.')
+
+        if(!validarId(idCliente)){
+            return res.status(400).json({name: 'Invalid Id', message: "Id de cliente inválido"})
+        }
+
+        if(!validarId(idStatus)){
+            return res.status(400).json({name: 'Invalid Id', message: "Id de status inválido"})
+        }
+
+        if(!eNumerico(valorFrete) || parseFloat(valorFrete) < 0){
+            return res.status(400).json({name: 'Validation Error', message: "Valor do frete inválido"})
+        }
+
+        valorFrete = parseFloat(valorFrete).toFixed(2)
+
+        if(!eNumerico(valorInstalacao) || parseFloat(valorInstalacao) < 0){
+            return res.status(400).json({name: 'Validation Error', message: "Valor de instalação inválido"})
+        }
+
+        valorInstalacao = parseFloat(valorInstalacao).toFixed(2)
+
+        itens = itens.map((item) => {
+            let {idAmbiente, idMaterial, quantidadeItem, comprimentoItem, larguraItem} = item
+
+            if(!idAmbiente || !idMaterial || !quantidadeItem || !comprimentoItem || !larguraItem){
+                return res.stauts(400).json({name: "Parameter Error", message: "Um dos itens não possui idOrcamento, idMaterial, idPreco, quantidadeItem, comprimentoItem, larguraItem"})
+            }
+
+            idAmbiente = idAmbiente.trim()
+            idMaterial = idMaterial.trim()
+            quantidadeItem = (typeof(quantidadeItem) === 'number') ? quantidadeItem.toString() : quantidadeItem.trim()
+            comprimentoItem = (typeof(comprimentoItem) === 'number') ? comprimentoItem.toString() : comprimentoItem.trim().replace(',', '.')
+            larguraItem = (typeof(larguraItem) === 'number') ? larguraItem.toString() : larguraItem.trim().replace(',', '.')
+
+            if(!validarId(idAmbiente)){
+                return res.status(400).json({name: "Invalid Id", message: "Id de ambiente inválido"})
+            }
+
+            if(!validarId(idMaterial)){
+                return res.status(400).json({name: "Invalid Id", message: "Id de material inválido"})
+            }
+
+            if(!eNumerico(quantidadeItem) || parseInt(quantidadeItem) <= 0){
+                return res.status(400).json({name: "Validation Error", message: "Quantidade inválida"})
+            }
+
+            if(!eNumerico(comprimentoItem) || parseFloat(comprimentoItem) <= 0){
+                return res.status(400).json({name: "Validation Error", message: "Comprimento inválido"})
+            }
+
+            comprimentoItem = parseFloat(comprimentoItem).toFixed(2)
+
+            if(!eNumerico(larguraItem) || parseFloat(larguraItem) <= 0){
+                return res.status(400).json({name: "Validation Error", message: "Largura inválida"})
+            }
+
+            larguraItem = parseFloat(larguraItem).toFixed(2)
+
+            return {idAmbiente, idMaterial, quantidadeItem, comprimentoItem, larguraItem}
+        })
+
+        const result = await orcamentoService.createOrcamento({idCliente, idStatus, valorFrete, valorInstalacao, itens})
+
+        return res.status(201).json({result})
+    }catch(error){
+        if(!error.status){
+            console.error(error)
+            return res.status(500).json({name: "Uncaugth Error", message: "Erro interno não tratado"})
+        }
+
+        return res.status(error.status).json({name: error.name, message: error.message})
     }
-
-    return res.status(resultado.status).json({message: resultado.message})
 }
 
 exports.getAll = async (_, res) => {
-    const resultado = await orcamentoService.getOrcamento()
+    try{
+        const result = await orcamentoService.getOrcamento()
 
-    if(resultado.status === 200){
-        return res.status(200).json({result: resultado.result})
+        return res.status(200).json({result})
+    }catch(error){
+        if(!error.status){
+            console.error(error)
+            return res.status(500).json({name: "Uncaugth Error", message: "Erro interno não tratado"})
+        }
+
+        return res.status(error.status).json({name: error.name, message: error.message})
     }
-
-    return res.status(resultado.status).json({message: resultado.message})
 }
 
 exports.getById = async (req, res) => {
-    const resultado = await orcamentoService.getOrcamentoById(req.params.id)
+    try{
+        const id = req.params.id.trim()
 
-    if(resultado.status === 200){
-        return res.status(200).json({result: resultado.result})
+        if(!validarId(id)){
+            return res.status(400).json({name: "Invalid Id", message: "Id inválido"})
+        }
+
+        const result = await orcamentoService.getOrcamentoById()
+
+        return res.status(200).json({result})
+    }catch(error){
+        if(!error.status){
+            console.error(error)
+            return res.status(500).json({name: "Uncaugth Error", message: "Erro interno não tratado"})
+        }
+
+        return res.status(error.status).json({name: error.name, message: error.message})
     }
-
-    return res.status(resultado.status).json({message: resultado.message})
 }
 
 exports.update = async (req, res) => {
-    const resultado = await orcamentoService.updateOrcamento({id: req.params.id, ...req.body})
+    try{
+        const id = req.params.id.trim()
 
-    if(resultado.status === 200){
-        return res.sendStatus(200)
+        if(!validarId(id)){
+            return res.status(400).json({name: "Invalid Id", message: "Id inválido"})
+        }
+
+        let {idCliente, idStatus, valorFrete, valorInstalacao, itens} = req.body
+
+        if(!idCliente || !idStatus || !valorFrete || !valorInstalacao || !itens){
+            return res.status(400).json({name: "Parameter Error", message: "idCliente, idStatus, valorPagamento, valorFrete, valorInstalacao e itens são obrigatóritos"})
+        }
+
+        idCliente = idCliente.trim()
+        idStatus = idStatus.trim()
+        valorPagamento = (typeof(valorPagamento) === 'number') ? valorPagamento.toString() : valorPagamento.trim().replace(',', '.')
+        valorFrete = (typeof(valorFrete) === 'number') ? valorFrete.toString() : valorFrete.trim().replace(',', '.')
+
+        if(!validarId(idCliente)){
+            return res.status(400).json({name: 'Invalid Id', message: "Id de cliente inválido"})
+        }
+
+        if(!validarId(idStatus)){
+            return res.status(400).json({name: 'Invalid Id', message: "Id de status inválido"})
+        }
+
+        if(!eNumerico(valorFrete) || parseFloat(valorFrete) < 0){
+            return res.status(400).json({name: 'Validation Error', message: "Valor do frete inválido"})
+        }
+
+        valorFrete = parseFloat(valorFrete).toFixed(2)
+
+        if(!eNumerico(valorInstalacao) || parseFloat(valorInstalacao) < 0){
+            return res.status(400).json({name: 'Validation Error', message: "Valor de instalação inválido"})
+        }
+
+        valorInstalacao = parseFloat(valorInstalacao).toFixed(2)
+
+        itens = itens.map((item) => {
+            let {idAmbiente, idMaterial, quantidadeItem, comprimentoItem, larguraItem} = item
+
+            if(!idAmbiente || !idMaterial || !quantidadeItem || !comprimentoItem || !larguraItem){
+                return res.stauts(400).json({name: "Parameter Error", message: "Um dos itens não possui idOrcamento, idMaterial, idPreco, quantidadeItem, comprimentoItem, larguraItem"})
+            }
+
+            idAmbiente = idAmbiente.trim()
+            idMaterial = idMaterial.trim()
+            quantidadeItem = (typeof(quantidadeItem) === 'number') ? quantidadeItem.toString() : quantidadeItem.trim()
+            comprimentoItem = (typeof(comprimentoItem) === 'number') ? comprimentoItem.toString() : comprimentoItem.trim().replace(',', '.')
+            larguraItem = (typeof(larguraItem) === 'number') ? larguraItem.toString() : larguraItem.trim().replace(',', '.')
+
+            if(!validarId(idAmbiente)){
+                return res.status(400).json({name: "Invalid Id", message: "Id de ambiente inválido"})
+            }
+
+            if(!validarId(idMaterial)){
+                return res.status(400).json({name: "Invalid Id", message: "Id de material inválido"})
+            }
+
+            if(!eNumerico(quantidadeItem) || parseInt(quantidadeItem) <= 0){
+                return res.status(400).json({name: "Validation Error", message: "Quantidade inválida"})
+            }
+
+            if(!eNumerico(comprimentoItem) || parseFloat(comprimentoItem) <= 0){
+                return res.status(400).json({name: "Validation Error", message: "Comprimento inválido"})
+            }
+
+            comprimentoItem = parseFloat(comprimentoItem).toFixed(2)
+
+            if(!eNumerico(larguraItem) || parseFloat(larguraItem) <= 0){
+                return res.status(400).json({name: "Validation Error", message: "Largura inválida"})
+            }
+
+            larguraItem = parseFloat(larguraItem).toFixed(2)
+
+            return {idAmbiente, idMaterial, quantidadeItem, comprimentoItem, larguraItem}
+        })
+
+        const result = await orcamentoService.updateOrcamento({id, idCliente, idStatus, valorFrete, valorInstalacao, itens})
+
+        return res.status(200).json(result)
+    }catch(error){
+        if(!error.status){
+            console.error(error)
+            return res.status(500).json({name: "Uncaugth Error", message: "Erro interno não tratado"})
+        }
+
+        return res.status(error.status).json({name: error.name, message: error.message})
     }
-
-    return res.status(resultado.status).json({message: resultado.message})
 }
 
 exports.delete = async (req, res) => {
-    const resultado = await orcamentoService.deleteOrcamento(req.params.id)
+    try{
+        const id = req.params.id.trim()
 
-    if(resultado.status === 200){
+        if(!validarId(id)){
+            return res.status(400).json({name: "Invalid Id", message: "Id inválido"})
+        }
+
+        await orcamentoService.deleteOrcamento(id)
+
         return res.sendStatus(200)
-    }
+    }catch(error){
+        if(!error.status){
+            console.error(error)
+            return res.status(500).json({name: "Uncaugth Error", message: "Erro interno não tratado"})
+        }
 
-    return res.status(resultado.status).json({message: resultado.message})
+        return res.status(error.status).json({name: error.name, message: error.message})
+    }
 }
