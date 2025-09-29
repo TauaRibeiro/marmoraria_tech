5f258f985039cf1ccb5d2562e1e651e6360c40f3
TauaRibeiro <tataumani@gmail.com>
2025-09-29 14:18:18 -0300

    UPDATE: statusConfig.js
    Adaptando o config para utilizar os status mutáveis e imutáveis

--- backend/config/statusConfig.js
+++ backend/config/statusConfig.js
@@ -1,13 +1,27 @@
 const Status = require('../models/Status')
 
 module.exports = async () => {
-    const statusPadroes = ['Ok', 'Estoque Baixo', 'Em aberto', 'Estoque Cheio', 'Estoque Transbordando', 'Aguardando Pagamento', 'Cancelado']
+    const statusMutaveis = ['Estoque Baixo', 'Ok', 'Em aberto', 'Estoque Cheio', 'Estoque Transbordando']
+    const statusImutaveis = ['Aguardando Pagamento', 'Cancelado']
     
-    statusPadroes.map(async (padrao) => {
-        let result = await Status.database.findOne({nome: padrao})
+    statusMutaveis.map(async (mutavel) => {
+        let result = await Status.database.findOne({nome: mutavel})
 
         if(!result){
-            const novoPadrao = new Status(padrao)
+            const novoPadrao = new Status(padrao, true)
+            await novoPadrao.create()
+
+            process.env[padrao.toUpperCase().replace(' ', '_')] = novoPadrao.id  
+        }else{
+            process.env[padrao.toUpperCase().replace(' ', '_')] = result._id
+        }
+    })
+
+    statusImutaveis.map(async (imutavel) => {
+        let result = await Status.database.findOne({nome: imutavel})
+
+        if(!result){
+            const novoPadrao = new Status(padrao, true)
             await novoPadrao.create()
 
             process.env[padrao.toUpperCase().replace(' ', '_')] = novoPadrao.id  
