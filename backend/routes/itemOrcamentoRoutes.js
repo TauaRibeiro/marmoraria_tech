4a95a9df3b82d6804bb66870048dd83b13ede9c8
TauaRibeiro <tataumani@gmail.com>
2025-09-29 13:28:55 -0300

    DELETE: itemOrcamentoController.js, itemOrcamentoRoutes.js e itemOrcamentoService.js

--- backend/routes/itemOrcamentoRoutes.js
+++
@@ -1,7 +0,0 @@
-const itemOrcamentoService = require('../controllers/itemOrcamentoController')
-const express = require('express')
-const router = express.Router()
-
-router.post('/', itemOrcamentoService.create)
-
-module.exports = router
