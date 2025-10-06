316e58edffdc07e8d2eebbae48a76ed24854deb1
TauaRibeiro <tataumani@gmail.com>
2025-10-06 17:36:17 -0300

    UPDATE: index.js
    Atualizando o router do sistema

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,33 @@
+import store from '@/store'
+import LoginPage from '@/views/loginPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/login',
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login-module',
+      component: LoginPage,
+      meta: { requiresGuest: true },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = store.getters['auth/isAuthenticated']
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login')
+  } else if (to.meta.requiresGuest && isAuthenticated) {
+    next('')
+  } else {
+    next()
+  }
 })
 
 export default router
