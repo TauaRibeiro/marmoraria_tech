2b5b7f1e6851ea8d141d59132ba7905429a9f611
TauaRibeiro <tataumani@gmail.com>
2025-10-21 21:05:49 -0300

    UPDATE: homePage.vue e orcamentosPage
    Começando a adicionar os elementos à pagina de orçamentos e preparando a área de escrolagem dos orçamentos. Também foi corrigido a identação do style de homePage.vue

--- frontend/src/views/homePage.vue
+++ frontend/src/views/homePage.vue
@@ -4,7 +4,7 @@
         <h2 class="sidebar__title">Marmoraria Tech</h2>
         <hr>
         <nav class="sidebar__menu">
-            <button class="sidebar-btn-active btn">Dashboard</button>
+            <button class="sidebar-btn btn">Dashboard</button>
             <button class="sidebar-btn btn">Orçamentos</button>
             <button class="sidebar-btn btn">Clientes</button>
             <button class="sidebar-btn btn">Materiais</button>
@@ -18,7 +18,6 @@
       <main class="content">
         <header class="content__header">
           <h1>Dashboard</h1>
-          <button class="btn primary-btn">Novo Orçamento</button>
         </header>
   
         <div class="cards">
@@ -80,203 +79,202 @@
     </div>
   </template>
   
-  <script>
-import router from '@/router';
-import store from '@/store';
+<script>
+  import router from '@/router';
+  import store from '@/store';
 
-    export default {
-      data() {
-        return {
-          
-        }
-      },
-      computed: {
-        loading(){
-          return store.getters.loading
-        },
-        orcamentos(){
-          return store.getters.orcamentos
-        },
-        error(){
-          return store.getters.error
-        },
-        numOrcamentosAbertos(){
-          let qtd = 0
-          
-          for(let i = 0; i < this.orcamentos.length; i++){
-            if(this.orcamentos[i].status === 'Aberto'){
-              qtd++
-            }
-          }
-          return qtd
-        },
-        numOrcamentosFinalizados(){
-          let qtd = 0
-
-          for(let i = 0; i < this.orcamentos.length; i++){
-            if(this.orcamentos[i].status === 'Finalizado'){
-              qtd++
-            }
-          }
-          return qtd
-        },
-        numOrcamentosEmAndamento(){
-          let qtd = 0
-
-          for(let i = 0; i < this.orcamentos.length; i++){
-            if(this.orcamentos[i].status === 'Em andamento'){
-              qtd++
-            }
-          }
-          return qtd
-        },
-        numOrcamentosTotal(){
-          return this.orcamentos.length
-        },
-      },
-      async mounted(){
-        console.log(this.loading)
-        const result = await store.dispatch('fetchOrcamentos')
-
-        if(result.status === 401 || result.status === 403){
-          alert(result.message)
-          store.dispatch('auth/logout')
-          router.push('login')
-        }
-
-      },
-      methods:{
-        logout(){
-          store.dispatch('auth/logout')
-          router.push('login')
-        },
-
+  export default {
+    data() {
+      return {
+        
       }
-    };
-    </script>
-    
-    <style scoped>
-    /* --- Reset simples --- */
-    /* * { margin: 0; padding: 0; box-sizing: border-box; font-family: Arial, sans-serif; } */
-    
-    .layout { 
-      display: flex; 
-      min-height: 100vh;
-      min-width: 100vh;
-      background: #f6f6f6; 
-      justify-content: space-between;
-      width: 100cqmax;
-    }
-    
-    /* --- Sidebar --- */
-    .sidebar { width: 280px; background: #fff; padding-top: 20px; padding-bottom: 20px; display: flex; flex-direction: column; border-right: 1px solid #eee; }
-    .sidebar__title { margin-bottom: 20px; margin-left: 10px }
-    .sidebar__menu { display: flex; flex-direction: column; gap: 10px; flex: 1; }
+    },
+    computed: {
+      loading(){
+        return store.getters.loading
+      },
+      orcamentos(){
+        return store.getters.orcamentos
+      },
+      error(){
+        return store.getters.error
+      },
+      numOrcamentosAbertos(){
+        let qtd = 0
+        
+        for(let i = 0; i < this.orcamentos.length; i++){
+          if(this.orcamentos[i].status === 'Aberto'){
+            qtd++
+          }
+        }
+        return qtd
+      },
+      numOrcamentosFinalizados(){
+        let qtd = 0
 
-    .sidebar-btn-active {
-      background-color: rgba(221, 221, 221, 0.466);
-    }
+        for(let i = 0; i < this.orcamentos.length; i++){
+          if(this.orcamentos[i].status === 'Finalizado'){
+            qtd++
+          }
+        }
+        return qtd
+      },
+      numOrcamentosEmAndamento(){
+        let qtd = 0
 
-    .sidebar-btn-active:active {
-      background-color: rgba(221, 221, 221, 0.466);
-    }
+        for(let i = 0; i < this.orcamentos.length; i++){
+          if(this.orcamentos[i].status === 'Em andamento'){
+            qtd++
+          }
+        }
+        return qtd
+      },
+      numOrcamentosTotal(){
+        return this.orcamentos.length
+      },
+    },
+    async mounted(){
+      console.log(this.loading)
+      const result = await store.dispatch('fetchOrcamentos')
 
-    .sidebar__link.active, .sidebar__link:hover { background: #f0f0f0; }
-    .sidebar__logout { margin-top: auto; padding: 8px; background: none; border: none; color: #d9534f; cursor: pointer; }
-    
-    /* --- Content --- */
-    .content {
-      flex: 1;
-      padding: 20px 40px;
-      width: 100vh;
-      margin: 0 auto; /* Centraliza no espaço disponível */
+      if(result.status === 401 || result.status === 403){
+        alert(result.message)
+        store.dispatch('auth/logout')
+        router.push('login')
       }
-    .content__header { display: flex; justify-content: space-between; align-items: center; margin-bottom: 20px; }
+
+    },
+    methods:{
+      logout(){
+        store.dispatch('auth/logout')
+        router.push('login')
+      },
+
+    }
+  };
+</script>
     
-    /*----Options----- */
-    .options {
-      display: flex;
-      gap: 15px;
-    }
-    
-    .btn-logout {
-      color: red;
-      width: 100%;
-      border-radius: 0;
-      margin: 0px;
-    }
-
-    .btn-logout:hover {
-      background-color: lightcoral;
-    }
-    
-    .btn {
-      border-radius: 5px;
-      padding: 10px;
-      text-align: left;
-    }
-    
-    .sidebar-btn {
-      border: none;
-      background-color: #fff;
-    }
-
-    .sidebar-btn:hover{
-      background-color: rgba(221, 221, 221, 0.466);
-    }
-
-    .btn-options {
-      padding-top: 1px;
-      padding-bottom: 1px;
-      padding-left: 5px;
-      padding-right: 5px;
-      border-style: solid;
-      border-color: rgba(221, 221, 221, 0.466);
-    }
-
-    .btn-options:hover {
-      background-color: rgba(221, 221, 221, 0.466);
-    }
-
-    .primary-btn {
-      background-color: rgb(2, 2, 87);
-      color: white;
-    }
-
-    .primary-btn:hover {
-      background-color: rgb(3, 3, 179);
-    }
-
-    .primary-btn:active {
-      color: white;
-      background-color: rgb(2, 2, 87);
-    }
-
-    .btn:active {
-      border-color: transparent;
-    }
-    /* --- Cards Dashboard --- */
-    .cards { display: grid; grid-template-columns: repeat(4, 1fr); gap: 15px; margin-bottom: 30px; }
-    .card { background: white; padding: 15px; border-radius: 8px; display: flex; flex-direction: column; }
-    .card__label { font-size: 14px; color: gray; }
-    .card__value { font-size: 22px; margin-top: 8px; font-weight: bold; }
-    
-    /* --- Tabela --- */
-    .table-section h2 { margin-bottom: 20px; }
-    .table-section {
-      background: white;
-      padding: 20px;
-      border-width: 1px;
-      border-radius: 10px;
-      border-color: rgba(212, 211, 211);
-      border-style: solid;
-    }
-    
-    .loading, .empty{
-      margin: auto;
-      text-align: center;
-      padding: 2rem;
-      color: #666;
-    }
-  </style>
+<style scoped>
+  /* --- Reset simples --- */
+  /* * { margin: 0; padding: 0; box-sizing: border-box; font-family: Arial, sans-serif; } */
   
+  .layout { 
+    display: flex; 
+    min-height: 100vh;
+    min-width: 100vh;
+    background: #f6f6f6; 
+    justify-content: space-between;
+    width: 100cqmax;
+  }
+  
+  /* --- Sidebar --- */
+  .sidebar { width: 280px; background: #fff; padding-top: 20px; padding-bottom: 20px; display: flex; flex-direction: column; border-right: 1px solid #eee; }
+  .sidebar__title { margin-bottom: 20px; margin-left: 10px }
+  .sidebar__menu { display: flex; flex-direction: column; gap: 10px; flex: 1; }
+
+  .sidebar-btn-active {
+    background-color: rgba(221, 221, 221, 0.466);
+  }
+
+  .sidebar-btn-active:active {
+    background-color: rgba(221, 221, 221, 0.466);
+  }
+
+  .sidebar__link.active, .sidebar__link:hover { background: #f0f0f0; }
+  .sidebar__logout { margin-top: auto; padding: 8px; background: none; border: none; color: #d9534f; cursor: pointer; }
+  
+  /* --- Content --- */
+  .content {
+    flex: 1;
+    padding: 20px 40px;
+    width: 100vh;
+    margin: 0 auto; /* Centraliza no espaço disponível */
+    }
+  .content__header { display: flex; justify-content: space-between; align-items: center; margin-bottom: 20px; }
+  
+  /*----Options----- */
+  .options {
+    display: flex;
+    gap: 15px;
+  }
+  
+  .btn-logout {
+    color: red;
+    width: 100%;
+    border-radius: 0;
+    margin: 0px;
+  }
+
+  .btn-logout:hover {
+    background-color: lightcoral;
+  }
+  
+  .btn {
+    border-radius: 5px;
+    padding: 10px;
+    text-align: left;
+  }
+  
+  .sidebar-btn {
+    border: none;
+    background-color: #fff;
+  }
+
+  .sidebar-btn:hover{
+    background-color: rgba(221, 221, 221, 0.466);
+  }
+
+  .btn-options {
+    padding-top: 1px;
+    padding-bottom: 1px;
+    padding-left: 5px;
+    padding-right: 5px;
+    border-style: solid;
+    border-color: rgba(221, 221, 221, 0.466);
+  }
+
+  .btn-options:hover {
+    background-color: rgba(221, 221, 221, 0.466);
+  }
+
+  .primary-btn {
+    background-color: rgb(2, 2, 87);
+    color: white;
+  }
+
+  .primary-btn:hover {
+    background-color: rgb(3, 3, 179);
+  }
+
+  .primary-btn:active {
+    color: white;
+    background-color: rgb(2, 2, 87);
+  }
+
+  .btn:active {
+    border-color: transparent;
+  }
+  /* --- Cards Dashboard --- */
+  .cards { display: grid; grid-template-columns: repeat(4, 1fr); gap: 15px; margin-bottom: 30px; }
+  .card { background: white; padding: 15px; border-radius: 8px; display: flex; flex-direction: column; }
+  .card__label { font-size: 14px; color: gray; }
+  .card__value { font-size: 22px; margin-top: 8px; font-weight: bold; }
+  
+  /* --- Tabela --- */
+  .table-section h2 { margin-bottom: 20px; }
+  .table-section {
+    background: white;
+    padding: 20px;
+    border-width: 1px;
+    border-radius: 10px;
+    border-color: rgba(212, 211, 211);
+    border-style: solid;
+  }
+  
+  .loading, .empty{
+    margin: auto;
+    text-align: center;
+    padding: 2rem;
+    color: #666;
+  }
+</style>
--- frontend/src/views/orcamentosPage.vue
+++ frontend/src/views/orcamentosPage.vue
@@ -1,62 +1,75 @@
 <template>
     <div class="layout">
-        <div class="orcamento-container d-flex w-100">
-            <aside class="sidebar">
-                <h2 class="sidebar__title">Marmoraria Tech</h2>
-                <hr>
-                <nav class="sidebar__menu">
-                    <button class="sidebar-btn btn">Dashboard</button>
-                    <button class="sidebar-btn-active btn">Orçamentos</button>
-                    <button class="sidebar-btn btn">Clientes</button>
-                    <button class="sidebar-btn btn">Materiais</button>
-                    <button class="sidebar-btn btn">Status</button>
-                    <button class="sidebar-btn btn">Funcionarios</button>
-                </nav>
-                <hr>
-                <button @click="logout" class="btn-logout btn">Sair</button>
-            </aside>
-            
+      <aside class="sidebar">
+        <h2 class="sidebar__title">Marmoraria Tech</h2>
+        <hr>
+        <nav class="sidebar__menu">
+            <button class="sidebar-btn btn">Dashboard</button>
+            <button class="sidebar-btn-active btn">Orçamentos</button>
+            <button class="sidebar-btn btn">Clientes</button>
+            <button class="sidebar-btn btn">Materiais</button>
+            <button class="sidebar-btn btn">Status</button>
+            <button class="sidebar-btn btn">Funcionarios</button>
+        </nav>
+        <hr>
+        <button @click="logout" class="btn-logout btn">Sair</button>
+      </aside>
+
+      <main class="content">
+        <header class="content__header">
+          <h1>Orçamentos</h1>
+          <button class="btn primary-btn">Novo Orçamento +</button>
+        </header>
+
+        <div class="search-bar">
+          <b>Id: </b> 
+          <input type="text">
         </div>
+
+        <div class="orcamentos-list">
+          <div>teste</div>
+          <div>teste</div>
+          <div>teste</div>
+          <div>teste</div> 
+          <div>teste</div> 
+          <div>teste</div> 
+          <div>teste</div> 
+          <div>teste</div> 
+          <div>teste</div> 
+          <div>teste</div> 
+          <div>teste</div> 
+          <div>teste</div>
+          <div>teste</div>
+        </div>
+      </main>
     </div>
 </template>
 
 <script>
+import Input from '@/components/Input.vue';
 export default {
   name: 'orcamentoPage',  
   data(){
   
   },
   components: {
+    Input,
   }
 }
 </script>
 
 <style scoped>
-  /* Configuração Layout */
   .layout { 
-      display: flex; 
-      min-height: 100vh;
-      min-width: 100vh;
-      background: #f6f6f6; 
-      justify-content: space-between;
-      width: 100cqmax;
+    display: flex; 
+    min-height: 100vh;
+    min-width: 100vh;
+    background: #f6f6f6; 
+    justify-content: space-between;
+    width: 100cqmax;
   }
-  /* Container*/
-  .orcamentos-container {
-      background-color: red;
-  }
-
-  /* Sidebar */
-  .sidebar { 
-      width: 280px; 
-      background: #fff; 
-      padding-top: 20px; 
-      padding-bottom: 20px; 
-      display: flex; 
-      flex-direction: column; 
-      border-right: 1px solid #eee;
-      height: 100%;
-  }
+  
+  /* --- Sidebar --- */
+  .sidebar { width: 280px; background: #fff; padding-top: 20px; padding-bottom: 20px; display: flex; flex-direction: column; border-right: 1px solid #eee; }
   .sidebar__title { margin-bottom: 20px; margin-left: 10px }
   .sidebar__menu { display: flex; flex-direction: column; gap: 10px; flex: 1; }
 
@@ -68,13 +81,34 @@ export default {
     background-color: rgba(221, 221, 221, 0.466);
   }
 
-  /* Botões */
+  .sidebar__link.active, .sidebar__link:hover { background: #f0f0f0; }
+  .sidebar__logout { margin-top: auto; padding: 8px; background: none; border: none; color: #d9534f; cursor: pointer; }
+  
+  /* --- Content --- */
+  .content {
+    flex: 1;
+    padding: 20px 40px;
+    width: 100vh;
+    margin: 0 auto; /* Centraliza no espaço disponível */
+    }
+  .content__header { 
+    display: flex; 
+    justify-content: space-between; 
+    align-items: center; 
+    margin-bottom: 20px; 
+  }
+  
+  /*----Options----- */
+  .options {
+    display: flex;
+    gap: 15px;
+  }
+  
   .btn-logout {
     color: red;
     width: 100%;
     border-radius: 0;
     margin: 0px;
-    align-self: flex-end;
   }
 
   .btn-logout:hover {
@@ -86,7 +120,7 @@ export default {
     padding: 10px;
     text-align: left;
   }
-
+  
   .sidebar-btn {
     border: none;
     background-color: #fff;
@@ -95,5 +129,67 @@ export default {
   .sidebar-btn:hover{
     background-color: rgba(221, 221, 221, 0.466);
   }
-  
+
+  .btn-options {
+    padding-top: 1px;
+    padding-bottom: 1px;
+    padding-left: 5px;
+    padding-right: 5px;
+    border-style: solid;
+    border-color: rgba(221, 221, 221, 0.466);
+  }
+
+  .btn-options:hover {
+    background-color: rgba(221, 221, 221, 0.466);
+  }
+
+  .primary-btn {
+    background-color: rgb(2, 2, 87);
+    color: white;
+  }
+
+  .primary-btn:hover {
+    background-color: rgb(3, 3, 179);
+  }
+
+  .primary-btn:active {
+    color: white;
+    background-color: rgb(2, 2, 87);
+  }
+
+  .btn:active {
+    border-color: transparent;
+  }
+
+  /* SEARCH BAR */
+  .search-bar {
+    background-color: white;
+    padding: 20px 10px 20px 10px;
+    border-radius: 8px;
+    border-style: solid;
+    border-color: rgba(212, 211, 211);
+    border-width: 1px;
+    margin-bottom: 30px;
+  }
+
+  input {
+    border-radius: 8px;
+    border-style: solid;
+    border-color: rgb(59, 59, 59);
+    border-width: 1px;
+  }
+
+  /* ORÇAMENTOS LIST */
+  .orcamentos-list {
+    position: relative;
+    background-color: white;
+    padding: 20px 10px 20px 10px;
+    border-radius: 8px;
+    border-style: solid;
+    border-color: rgba(212, 211, 211);
+    border-width: 1px;
+    max-height: 300px;
+    overflow-y: auto;
+  }
+
 </style>
