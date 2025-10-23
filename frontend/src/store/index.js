b5824e3d759de8f38431c7d0f3d0cdbdd5a47e7e
TauaRibeiro <tataumani@gmail.com>
2025-10-23 08:40:43 -0300

    UPDATE: index.js
    Adicionando o store de status ao index.js do store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex'
 import auth from './modules/auth'
 import orcamentos from './modules/orcamentos'
+import status from './modules/status'
 
 export default createStore({
   modules: {
     auth,
     orcamentos,
+    status,
   },
 })
