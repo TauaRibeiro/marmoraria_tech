8321bcd1fa1753961b3f36c07853a4c645f4053a
Aluno Lauro de Freitas <tataumani@gmail.com>
2025-11-27 20:04:15 -0300

    UPDATE: index.js
    Adicionando os store de materiais e clientes ao index de store/

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,15 @@ import { createStore } from 'vuex'
 import auth from './modules/auth'
 import orcamentos from './modules/orcamentos'
 import status from './modules/status'
+import materiais from './modules/materiais'
+import clientes from './modules/clientes'
 
 export default createStore({
   modules: {
     auth,
     orcamentos,
     status,
+    materiais,
+    clientes
   },
 })
