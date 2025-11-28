15d59f58de4f2d495fe901b99326450dd67e511e
Aluno Lauro de Freitas <tataumani@gmail.com>
2025-11-27 21:13:26 -0300

    CREATE: Dropdown.vue e newOrcamentoPage.vue
    Criando o componente de  seleção dropdown e inicio a da criação da página de novo orcamento

---
+++ frontend/src/components/Dropdown.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="drop-container">
+        <label :for="id">
+            {{ label }}
+        </label>
+        <select 
+            class="form-select" :id="id"
+            @select="$emit('update:modelValue', $event.target.value)"
+        >
+            <option value="" selected disabled>{{ placeholder }}</option>
+            <slot></slot>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DropdownComponent",
+        props: {
+            id: {
+                type: String,
+                required: true,
+            },
+            label: {
+                type: String,
+                default: ""
+            },
+            placeholder: {
+                type: String,
+                default: "Selecione"
+            }
+        },
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ frontend/src/views/newOrcamentoPage.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="layout">
+        <div class="content">
+            <header class="">
+                <h2>Orçamentos</h2>
+                <button>Voltar</button>
+            </header>
+            <form @submit.prevent= "">
+                <h2>Informações Orçamento</h2>
+    
+                <div id="clientes">
+                    <Dropdown id="cliente-value" label="Cliente" placeholder="Selecione um cliente">
+                        <option value="">Teste</option>
+                    </Dropdown>
+                </div>
+    
+                <div id="">
+    
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Dropdown from '@/components/Dropdown.vue';
+
+    export default{
+        components: {
+            Dropdown
+        }
+    }
+</script>
+
+<style scoped>
+    .layout { 
+        display: flex; 
+        min-height: 100vh;
+        min-width: 100vh;
+        background: #f6f6f6; 
+        justify-content: space-between;
+        width: 100cqmax;
+    }
+
+
+</style>
